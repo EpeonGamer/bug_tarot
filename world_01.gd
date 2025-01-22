70fd56c7f20c909087feceea9236984d27bb1253
Epeon <60582251+EpeonGamer@users.noreply.github.com>
2025-01-22 23:54:30 +0200

    Updated more aspect resource defaults
    Elements and psyches now also included. world_01 test scene modified to reflect these changes.

--- world_01.gd
+++ world_01.gd
@@ -2,18 +2,22 @@ extends Node2D
 
 @onready var card_ratio: CardContainer = %card_ratio
 
-var bug_icon = card_ratio.bug_icon
-var element_icon = card_ratio.element_icon
-var psyche_icon = card_ratio.psyche_icon
+var bug_icon : TextureRect
+var element_icon : TextureRect
+var psyche_icon : TextureRect
 
 var player_1_stats : PlayerStats = PlayerStats.new()
 @export var initial_card : BaseCard = BaseCard.new()
 
 func _ready() -> void:
+	var bug_icon : TextureRect = card_ratio.bug_icon
+	var element_icon : TextureRect = card_ratio.element_icon
+	var psyche_icon : TextureRect = card_ratio.psyche_icon
+	
 	var second_card = BaseCard.new()
 	second_card.bug_resource = preload("res://card_aspects/bug_aspects/earwig_base.tres")
-	second_card.bug_resource = preload("res://card_aspects/bug_aspects/earwig_base.tres")
-	second_card.bug_resource = preload("res://card_aspects/bug_aspects/earwig_base.tres")
+	second_card.element_resource = preload("res://card_aspects/element_aspects/illusion_base.tres")
+	second_card.psyche_resource = preload("res://card_aspects/psyche_aspects/wrath_base.tres")
 	
 	var initial_cards : Array = [initial_card]
 	player_1_stats._move_single_card(initial_cards,player_1_stats.hand)
